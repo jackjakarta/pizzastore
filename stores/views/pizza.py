c2e80bbd54f2cfb91fcbb537838b5ca33b24d453
jackjakarta <al.termure@gmail.com>
2024-02-12 19:05:46 +0100

    pagination step 1

--- stores/views/pizza.py
+++ stores/views/pizza.py
@@ -1,24 +1,17 @@
+from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
-from django.shortcuts import render
 from django.views.generic import ListView
-
 from stores.models import Pizza
 
 
-class PizzaList(ListView):
-    model = Pizza
-    context_object_name = 'pizza_list'
-    template_name = 'stores/pizza/list.html'
-    paginate_by = 6
-
-
 def pizza_list_view(request):
-    pizza_list = Pizza.objects.all()
+    pizza_list = Pizza.objects.all().order_by('price')
     paginator = Paginator(pizza_list, 9)
 
-    page_number = request.GET.get("page", 1)
+    page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
+    print(page_obj)
 
-    return render(request, "stores/pizza/list.html", {
-        "page_obj": page_obj,
+    return render(request, 'stores/pizza/list.html', {
+        'page_obj': page_obj,
     })
